a5ac234c4f21494d917e449b6b18700e0dbf143c
MrLucasFischer <lucasfischerpt@gmail.com>
2020-03-14 13:39:52 +0000

    Updates

---
+++ precache-manifest.5b5f0a2013ea99cd9809a7796e0acd71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3ad1b55e327cdf0494c1e250fc4cdf",
+    "url": "/cardosoepalma/index.html"
+  },
+  {
+    "revision": "20f089fd764012ce4678",
+    "url": "/cardosoepalma/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2bf67c4d5b325542d09",
+    "url": "/cardosoepalma/static/js/2.40a8be54.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cardosoepalma/static/js/2.40a8be54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f089fd764012ce4678",
+    "url": "/cardosoepalma/static/js/main.b6d9d700.chunk.js"
+  },
+  {
+    "revision": "9f0cdaec7e39b1bec069",
+    "url": "/cardosoepalma/static/js/runtime-main.e3950ee3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cardosoepalma/static/media/logo.5d5d9eef.svg"
+  }
+]);
