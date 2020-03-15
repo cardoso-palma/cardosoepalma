343e4fe4daed6cf2883a0f037b2c2b84776af25d
MrLucasFischer <lucasfischerpt@gmail.com>
2020-03-15 17:52:38 +0000

    Creating homepage component

---
+++ src/Homepage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+    render() {
+        return (
+            <h1>Hi</h1>
+        );
+    }
+}
+
+// <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     {test()}
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+export default Homepage;
