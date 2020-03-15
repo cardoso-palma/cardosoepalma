c9401cccac480aa9971f9739e3365c8964c9f61f
MrLucasFischer <lucasfischerpt@gmail.com>
2020-03-15 20:40:35 +0000

    changed about us and init div

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -3,7 +3,17 @@ import React from 'react';
 class AboutUs extends React.Component {
     render() {
         return (
-            <p>AboutUs</p>
+            <div >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum mi nunc, id aliquet tellus aliquet ac. Ut ut odio lacinia, pharetra lacus eget, vulputate leo. Nulla mollis tellus a eleifend eleifend. Donec vel molestie velit, sed convallis velit. Proin varius commodo erat, sit amet auctor ante venenatis in. Suspendisse in ex ipsum. Maecenas at molestie nibh. Aenean non pretium ligula. Aenean bibendum leo ut ex maximus, auctor imperdiet lorem viverra. Praesent commodo facilisis quam sit amet dignissim. Praesent ut sapien vel turpis consequat vestibulum ornare nec felis.
+            
+            Proin tortor ipsum, maximus vel dui sed, vestibulum tincidunt mi. Fusce libero mi, fringilla ultrices libero vel, rutrum finibus justo. Vestibulum maximus, ante et consectetur porta, neque ligula consequat odio, ac tempus lorem lorem consectetur urna. Etiam dapibus turpis eu risus vulputate vulputate. Pellentesque sed lobortis urna. Aliquam facilisis urna aliquet erat tristique finibus. Integer a ipsum vitae augue laoreet viverra.
+            
+            Nunc dictum velit eu nulla ultricies, id dignissim nisl imperdiet. Nulla ut mattis odio. Suspendisse finibus nisi sapien, non dignissim leo pharetra at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse sed orci pretium, sollicitudin lectus sit amet, gravida lorem. In eu felis commodo elit posuere viverra condimentum nec nisi. Curabitur ullamcorper condimentum tincidunt. Curabitur hendrerit venenatis ipsum.
+            
+            Donec dictum diam sit amet tortor faucibus condimentum. Curabitur sed ex blandit, gravida purus eget, vestibulum ipsum. Vestibulum nec nulla eu libero sagittis fermentum. Vestibulum tristique luctus dignissim. Phasellus cursus mauris leo. Sed egestas eget massa at porta. Sed non nunc vestibulum, molestie ex et, eleifend elit. Cras eu ante odio. Curabitur non dolor leo. Maecenas consectetur tempus arcu sed semper. Nam egestas turpis eu felis faucibus convallis. Nam ultricies convallis magna quis lacinia. Vestibulum in varius dolor. Pellentesque cursus luctus elit, at consequat tellus maximus sit amet.
+            
+            Vestibulum ullamcorper, libero at placerat porta, risus tortor feugiat ligula, tincidunt efficitur est arcu a purus. Praesent faucibus malesuada lacus, at aliquet diam tempus eget. Aenean metus elit, accumsan rhoncus blandit sed, mattis et quam. Morbi at faucibus sapien. Nam sit amet nisl ut lectus efficitur finibus. Etiam malesuada quam id facilisis finibus. Cras sapien urna, ultricies rutrum ex sed, posuere elementum orci.
+            </div>
         );
     }
 }
