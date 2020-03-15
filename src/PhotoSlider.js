083efad1309b03194d602df48f2f7d91a7caf4e6
MrLucasFischer <lucasfischerpt@gmail.com>
2020-03-15 19:35:46 +0000

    Working on Photo Component

--- src/PhotoSlider.js
+++ src/PhotoSlider.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Button from './Button'
 import './photoslider.css'
 
 class PhotoSlider extends React.Component {
     render() {
         return (
-            <div id="photo-div">
-                Stuff
+            <div className="centered photo-div">
+                <div className="centered title-div">
+                    <h1 className="subtitle">An impactful phrase here</h1>
+                    <h3 className="subtitle">Uma empresa pequenina e aqui estou só a escrever mais coisas para ver se faz overflow</h3>
+                    <Button buttonText={'Saber Mais'} />
+                </div>
             </div>
         );
     }
