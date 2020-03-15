74b5fe2f3626fe79e4afb640b3606b6021b0880e
andreia-pereira <andreiiap02@hotmail.com>
2020-03-15 20:12:15 +0000

    menu options underlying

--- src/Menu.js
+++ src/Menu.js
@@ -6,13 +6,14 @@ class Menu extends React.Component {
         return (
             <div className='menu'>
                 <div id='logo'>
+                
                     <img src={require('./logoteste.png')}></img>
 
                 </div>
                 <div id='menuOptions'>
-                    <a href="#"> About Us</a>
-                    <a href="#"> Portfolio </a>
-                    <a href="#"> Contact Us</a>
+                    <a href="#"> <span> About Us </span></a>
+                    <a href="#"> <span> Portfolio </span></a>
+                    <a href="#"> <span> Contact Us </span></a>
                 </div>
             </div>
         );
