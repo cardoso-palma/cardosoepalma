1cbdf448d1372c6400c91f232f55c741109acd00
andreia-pereira <andreiiap02@hotmail.com>
2020-03-15 19:05:18 +0000

    align menu options

--- src/Menu.js
+++ src/Menu.js
@@ -4,7 +4,16 @@ import './menu.css'
 class Menu extends React.Component {
     render() {
         return (
-            <div className='menu'>Menu</div>
+            <div className='menu'>
+                <div id='logo'>
+                    Logotipo
+                </div>
+                <div id='menuOptions'>
+                    <a href="#"> About Us</a>
+                    <a href="#"> Portfolio </a>
+                    <a href="#"> Contact Us</a>
+                </div>
+            </div>
         );
     }
 }
